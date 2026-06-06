77c27ba1b9e98bf9be73bc96066cf43b93e262f6
Melanie Rodriguez <melarodrigg@gmail.com>
2026-06-06 13:50:46 -0400

    Updated GET Matches (API Fetch) to return only World Cup Events. Recorded Postman response to prevent further calls for now.Built Home Page which includes MatchesList

--- src/pages/MatchesList.jsx
+++ src/pages/MatchesList.jsx
@@ -1,9 +1,35 @@
-
+import { useEffect, useState } from "react"
+import { Outlet } from "react-router-dom"
+import { getMatches } from "../services/GetMatches"
 
 function MatchesList() {
+  const [matches, setMatches] = useState([]);
+
+  let today = new Date().toISOString().split("T")[0]
+  let kickoff = `2026-06-11`
+
+  useEffect( () => {
+    getMatches(kickoff).then( (data) => {
+      setMatches(data)
+    }).catch( (error) => {
+      console.log("Error fetching matches: ", error)
+    });
+  }, []);
+
+  if (matches.length === 0){
+    return <p>Loading Events...</p>
+  }
+  const cards = matches.map(match => (
+    <li key={match.id} className="match-card">
+      {match.homeTeam} vs {match.awayTeam}
+    </li>
+  ))
+
+
   return(
-    <>
-    </>
+    <ul>
+      {cards}
+    </ul>
   )
 }
 
--- src/services/GetMatches.js
+++ src/services/GetMatches.js
@@ -3,14 +3,12 @@
 //// Installed dotenv package: `npm install dotenv`
 //// Added .env file to project root to store environment variables locally
 //// Loaded environment variables from .env file:
-import 'dotenv/config.js'
 
 // Base API URL
 const baseURL = 'https://sportapi7.p.rapidapi.com/api/v1/sport/football/scheduled-events';
 
 // API Key
-const apiKey = process.env.RAPIDAPI_KEY;
-console.log(apiKey)
+const apiKey = import.meta.env.VITE_RAPIDAPI_KEY;
 
 // Date Handling:
 // new Date() provides today's date
@@ -19,30 +17,45 @@ console.log(apiKey)
 
 // 'Today' with YYYY-MM-DD format
 const today = new Date().toISOString().split("T")[0];
-console.log(today)
+//  
 
-// GET Matches by given date
+// GET Matches by given date (date to pass as parameter)
 export async function getMatches(date) {
   const url = `${baseURL}/${date}`
+  console.log(url)
 
   if (date) {
     const response = await fetch(url, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "x-rapidapi-key": apiKey,
+        "x-rapidapi-key": apiKey, // Uses apiKey declared above, stored in a separate .env file
         "x-rapidapi-host": "sportapi7.p.rapidapi.com"
       }
     });
     if (response.ok) {
       const data = await response.json();
-      return data;
+
+      // Filter response by events that match FIFA World Cup (In SportsAPI, the matching key is id 16)
+      // Maps worldCupMatches to keys needed for state management      
+      const worldCupMatches = data.events.filter(
+        event => event.tournament?.uniqueTournament?.id === 16
+      )
+      .map(event => ({
+        id: event.id,
+        homeTeam: event.homeTeam.name,
+        awayTeam: event.awayTeam.name,
+        startTimestamp: event.startTimestamp,
+        group: event.tournament.groupName,
+        round: event.roundInfo.round
+      }))
+
+      // async function fetches API response returns only 2 to 4 events needed per day (date)
+      return worldCupMatches;
     }
     throw new Error(`Error fetching matches: ${response.statusText}`);
     return null
   }
-  
-  else {
-    console.log('Missing Date')
-  }
+
+
 }
