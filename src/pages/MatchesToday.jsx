ea08c9055f874a4d4b28d8b508ce176bba0c3339
Melanie Rodriguez <melarodrigg@gmail.com>
2026-06-10 19:56:22 -0400

    Minor update to MatchesToday. It will now show Stay Tuned! if event has not gone live

--- src/pages/MatchesToday.jsx
+++ src/pages/MatchesToday.jsx
@@ -4,6 +4,16 @@ import MatchGrid from "../components/MatchGrid"
 function MatchesToday() {
   const { matches} = useOutletContext();
 
+  // Requires further development
+  // If event has not started, display events on kickoff date
+
+  const today = new Date()
+  const kickoff = new Date(`2026-06-11T00:00:00`)
+
+  if (kickoff > today) {
+    return <p>Stay Tuned!</p>
+  }
+
   if (matches.length === 0){
     return <p>Loading Events...</p>
   }
