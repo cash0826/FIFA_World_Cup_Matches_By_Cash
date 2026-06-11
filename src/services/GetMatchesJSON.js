f58ee4b426e5efe6db91f2afe1918400e24b451b
Melanie Rodriguez <melarodrigg@gmail.com>
2026-06-11 16:58:38 -0400

    Recorded a new call response in txt and response.json using a new future date to test API calls on a new date without calling API each time. Fixed small bug in GetMatch to filter by tournment id and filter matches by converted UNIX timestamps to local time

--- src/services/GetMatchesJSON.js
+++ src/services/GetMatchesJSON.js
@@ -11,7 +11,19 @@ export async function getMatches() {
   if (response.ok) {
     const data = await response.json();
 
-    const worldCupMatches = data.map((event) =>({
+    const startOfDay = new Date(`2026-06-13T00:00:00`);
+    const endOfDay = new Date(`2026-06-13T23:59:59`);
+    
+    // Convert to UNIX timestamps (API uses seconds, not milliseconds)
+    const startTimestamp = Math.floor(startOfDay.getTime() / 1000);
+    const endTimestamp = Math.floor(endOfDay.getTime() / 1000);
+
+    const worldCupMatches = data.filter(
+      event => event.tournament?.uniqueTournament?.id === 16 &&
+                event.startTimestamp >= startTimestamp &&
+                event.startTimestamp <= endTimestamp
+    )
+    .map((event) =>({
       id: event.id,
       homeTeam: event.homeTeam.name,
       awayTeam: event.awayTeam.name,
