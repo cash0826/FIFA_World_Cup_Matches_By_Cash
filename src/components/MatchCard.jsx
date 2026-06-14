e8744f87b63b3ff400dc5ee754d7670c0f456d2b
Melanie Rodriguez <melarodrigg@gmail.com>
2026-06-13 23:44:27 -0400

    Recorded teams from API response and stored teams as object with corresponding flag file name. Added dynamic path to MatchCard in order to display flags

--- src/components/MatchCard.jsx
+++ src/components/MatchCard.jsx
@@ -1,13 +1,40 @@
 import '../styles/MatchCard.css'
 import { formatTimeStamp } from "../utils/formatTimeStamp"
+import { teams } from "../utils/teams"
 
 function MatchCard( { match } )  {
   
   // Format startTimestamp (UNIX) to display Local Time (separated to utils)
 
+  // Get team data from teams array to access flag filename
+  const homeTeam = teams.find(team => team.name === match.homeTeam)
+  const homeTeamFlag = homeTeam ? new URL(`../assets/flags/${homeTeam.flag}`, import.meta.url).href : ''
+
+  const awayTeam = teams.find(team => team.name === match.awayTeam)
+  const awayTeamFlag = awayTeam ? new URL(`../assets/flags/${awayTeam.flag}`, import.meta.url).href : ''
+
   return(
     <div className="match-card-div">
-      <h3 className="match-title">{match.homeTeam} | {formatTimeStamp(match.startTimestamp)} | {match.awayTeam} </h3>
+      <h3 className="match-title">
+        <img
+          className="flag"
+          src={homeTeamFlag}
+          title={match.homeTeam}
+          alt={match.homeTeam}
+        />
+        {match.homeTeam} 
+
+        | {formatTimeStamp(match.startTimestamp)} 
+
+        <img
+          className="flag"
+          src={awayTeamFlag}
+          title={match.awayTeam}
+          alt={match.awayTeam}
+        />
+        | {match.awayTeam} 
+      </h3>
+      
       <h4 className="match-details"> Round {match.round} | {match.group} </h4>
     </div>
   )
