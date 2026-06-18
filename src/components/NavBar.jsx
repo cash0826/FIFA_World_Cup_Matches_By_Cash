b53f15b420fedee613645b30c60b637ec09e3722
Melanie Rodriguez <melarodrigg@gmail.com>
2026-06-17 22:00:24 -0400

    Final update to useEffect on NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,11 @@ import '../styles/NavBar.css'
 import trionda from '../assets/ball-trionda-2026.png'
 import CountDownClock from "../components/CountDownClock"
 
+// Moved kickoff to outside the component so useEffect does not change it between renders
+// World Cup 2026 Kick Off: First Match JUN 11
+const kickoff = new Date(`2026-06-11T15:00:00`)
+
 function NavBar() {
-  // World Cup 2026 Kick Off: First Match JUN 11
-  const [ kickoff, setKickoff ] = useState(new Date(`2026-06-11T15:00:00`))
   // Lazy initializer to return initial state
   const [ countdown, setCountdown ] = useState(() => ({
     days: 0,
