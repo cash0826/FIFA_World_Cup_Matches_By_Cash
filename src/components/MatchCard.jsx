69e3718d0e976bd77b8563d4005f372c525cacf7
Melanie Rodriguez <melarodrigg@gmail.com>
2026-06-18 21:59:29 -0400

    Added conditional rendering of scores if game has ended or started

--- src/components/MatchCard.jsx
+++ src/components/MatchCard.jsx
@@ -1,6 +1,7 @@
 import '../styles/MatchCard.css'
 import { formatTimeStamp } from "../utils/formatTimeStamp"
 import { teams } from "../utils/teams"
+import emblem from '../assets/WC2026_emblem.svg.webp'
 
 function MatchCard( { match } )  {
   // Get team data from teams object to access flag filename
@@ -11,38 +12,55 @@ function MatchCard( { match } )  {
   const awayTeamFlag = awayTeam ? new URL(`../assets/flags/${awayTeam.flag}`, import.meta.url).href : ''
 
   return(
-    <div className="match-card-div">
-      <div className="match-homeTeam">
-        <img
-          className="flag"
-          src={homeTeamFlag}
-          title={match.homeTeam}
-          alt={match.homeTeam}
-        />
-        <h4>{match.homeTeam}</h4> 
-      </div>
+    <div className="match-card-main">
+      <div className="match-card-div">
+        <div className="match-homeTeam">
+          <img
+            className="flag"
+            src={homeTeamFlag}
+            title={match.homeTeam}
+            alt={match.homeTeam}
+          />
+          <h4>{match.homeTeam}</h4> 
+        </div>
 
-      <div className="match-details">
-        <h4>{formatTimeStamp(match.startTimestamp)}</h4>
-        <br/>
-        <p>Group {match.group}</p>
-        {match.status !== "Not started" && (
-          <p id="live"> {match.status} </p>
-        )}
+        <div className="match-details">
+          <h4>{formatTimeStamp(match.startTimestamp)}</h4>
+          <p>Group {match.group}</p>
+          {match.status !== "Not started" && (
+            <p id="status"> {match.status} </p>
+          )}
+          {match.inProgress === "inprogress" && (
+            <p>🟢</p>
+          )}          
+        </div>
 
+        <div className="match-awayTeam">
+          <img
+            className="flag"
+            src={awayTeamFlag}
+            title={match.awayTeam}
+            alt={match.awayTeam}
+          />
+          <h4>{match.awayTeam}</h4> 
+        </div>
       </div>
-
-      <div className="match-awayTeam">
-        <img
-          className="flag"
-          src={awayTeamFlag}
-          title={match.awayTeam}
-          alt={match.awayTeam}
-        />
-        <h4>{match.awayTeam}</h4> 
-      </div>
-      
-      
+      {match.status !== "Not started" && (
+        <div className="match-card-div">
+          <div className="match-score">
+            <h4 className="country-short-code">{match.homeTeamCode}</h4>
+            <h3 className="score">{match.homeScore}</h3>
+            <img
+              className="emblem"
+              src={emblem}
+              title="2026 FIFA World Cup Emblem"
+              alt="2026 FIFA World Cup Emblem"
+            />
+            <h3 className="score">{match.awayScore}</h3>
+            <h4 className="country-short-code">{match.awayTeamCode}</h4>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
